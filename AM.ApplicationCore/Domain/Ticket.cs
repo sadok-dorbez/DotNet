78664621e9dbab8e55ccaa382e375494b27bf462
sadok-dorbez <sadokdorbez0@gmail.com>
2023-04-19 02:12:47 +0100

    principe et patron de conception

---
+++ AM.ApplicationCore/Domain/Ticket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Domain
+{
+    public class Ticket
+    {
+        public double Prix { get; set; }
+        public int Siege { get; set; }
+        public bool VIP { get; set; }
+        public virtual string PassengerFK { get; set; }
+        public virtual Passenger Passenger { get; set; }
+        public virtual int FlightFK { get; set; }
+        public virtual Flight Flignt { get; set; }
+    }
+}
---
+++ AM.UI.WEBA/Controllers/FlightController.cs
@@ -0,0 +1,123 @@
+﻿using AM.ApplicationCore.Domain;
+using AM.ApplicationCore.Interfaces;
+using AM.ApplicationCore.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AM.UI.WEBA.Controllers
+{
+    public class FlightController : Controller
+    {
+        // GET: FlightController
+        private IServiceFlight serviceFlight;
+        private IServicePlane servicePlane;
+        public FlightController(IServiceFlight serviceFlight, IServicePlane servicePlane)
+        {
+            this.serviceFlight = serviceFlight;
+            this.servicePlane = servicePlane;   
+        }
+        public ActionResult Index(string destination)
+        {
+
+            var list = serviceFlight.GetAll().ToList();
+            if (destination != null)
+            {
+                list = list.Where(f => f.Destination.Contains(destination)).ToList();
+            }
+            return View(list);
+        }
+       
+
+
+        // GET: FlightController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+        public ActionResult Search(string destination)
+        {
+            return View("index", serviceFlight.GetAll().Where(f => f.Destination.Contains(destination)));
+        }
+        // GET: FlightController/Create
+        public ActionResult Create()
+        {
+            ViewBag.Planes =new SelectList(servicePlane.GetAll(), "PlaneId", "Information");
+            return View();
+        }
+
+        // POST: FlightController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Flight collection, IFormFile piloteFile)
+        {
+            try
+            {
+                if (piloteFile!=null)
+                {
+                    var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "img", piloteFile.FileName);
+                    var stream = new FileStream(path, FileMode.Create);
+                    piloteFile.CopyTo(stream);
+                    collection.Pilote = piloteFile.FileName;
+                }
+             
+
+                serviceFlight.Add(collection);
+                serviceFlight.Commit();
+                return RedirectToAction(nameof(Index));
+                
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FlightController/Edit/5
+        public ActionResult Edit(int id)
+            
+        {
+            var flight =serviceFlight.GetById(id);
+            ViewBag.Planes = new SelectList(servicePlane.GetAll(), "PlaneId","Information");
+            return View(flight);
+        }
+
+        // POST: FlightController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Flight collection)
+        {
+            try
+            {
+                serviceFlight.Update(collection);
+                serviceFlight.Commit();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FlightController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: FlightController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
